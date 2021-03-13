dd6ce18673378f7b25c74c225a2f2cdd555a48a0
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-03-13 12:55:20 +0700

    a

--- Crawling/crawling.py
+++ Crawling/crawling.py
@@ -1,13 +1,13 @@
 import twint
 
 twi = twint.Config()
-twi.Search = 'dukung vaksin corona'
+twi.Search = 'vaksin corona'
 twi.Lang = 'in'
 twi.Until = '2021-02-18'
 twi.Since = '2021-01-13'
 Limit = 1000
 Pandas = True
 twi.Custom["tweet"] = ["id", "date", "username", "tweet"]
-twi.Output = "vaksin2.csv"
+twi.Output = "vaksin.csv"
 twi.Store_csv = True
 twint.run.Search(twi)
